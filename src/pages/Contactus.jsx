58dc65a590a331cb4a67427f2eb66ec73b1b2c83
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2025-02-19 00:37:39 +0530

    cont

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -24,7 +24,7 @@ export default function ContactUs() {
           className="mt-8 bg-gray-800 p-6 rounded-2xl shadow-lg"
         >
           <h2 className="text-2xl font-semibold text-yellow-400">📍 Our Address</h2>
-          <p className="text-gray-300 mt-4">123 TyreCare Street, Delhi, India</p>
+          <p className="text-gray-300 mt-4">14 KM Umrangso, Dima Hasao, India</p>
         </motion.div>
 
         <motion.div
@@ -34,7 +34,7 @@ export default function ContactUs() {
           className="mt-8 p-6 border-l-4 border-yellow-400"
         >
           <h2 className="text-2xl font-semibold text-yellow-400">📞 Contact Number</h2>
-          <p className="text-gray-300 mt-4">+91 98765 43210</p>
+          <p className="text-gray-300 mt-4">+91 75420 67763</p>
         </motion.div>
 
         <motion.div
@@ -44,7 +44,7 @@ export default function ContactUs() {
           className="mt-8 bg-gray-800 p-6 rounded-2xl shadow-lg"
         >
           <h2 className="text-2xl font-semibold text-yellow-400">✉ Email Us</h2>
-          <p className="text-gray-300 mt-4">support@tyrecare.com</p>
+          <p className="text-gray-300 mt-4">gausiyatyreworks@gmail.com</p>
         </motion.div>
 
         <motion.div
